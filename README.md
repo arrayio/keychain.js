36446542277395f479e11fef1de4024f31aa114e
dtaipov <dtaipov@gmail.com>
2019-02-22 11:35:37 +0300

    Keychain.js initial version

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# keychain.js-
+
+
+[![npm version](https://badge.fury.io/js/web3override.svg)](https://badge.fury.io/js/web3override)
+
+* `bitcoin.js` - extend `bitcoinjs-lib` with keychain related methods `prepareTx`, `buildTxKeychain`
+* `keychain.js` - Keychain class with ws connection initialization
+* `index.js` - override `web3.eth.accounts.signTransaction` method 
+* `test.js` - example usage together (`keychain` + `web3`) 
+
+**Usage**
+
+* Sign ethereum transaction 
+```javascript
+
+  import { web3Override, Keychain } from 'web3override'
+
+  // create new key in Keychain
+  const keyInstance = await Keychain.create();
+   
+  const data = await keyInstance.selectKey();
+  const key = data.result;
+  await keyInstance.term();
+  web3.eth.accounts.signTransaction = web3Override(web3).signTransaction;
+
+  // now we use web3 with keychain
+  await web3.eth.accounts.signTransaction(transactionParams, key); // overriden web3 function usage
+```
+
+* Sign & build ready for broadcast bitcoin transaction 
+
+```javascript
+  // import library
+  import { bitcoin, Keychain } from 'web3override';
+
+  // tx - bitcoin.TransactionBuilder instance with inputs & outputs
+  const keyInstance = await Keychain.create();
+  const data = await keyInstance.selectKey();
+  const publicKey = data.result;
+  tx.inputs.forEach((input, index) => {
+    tx.prepareTx(index,  publicKey)
+  });
+  const txRawHex = await tx.buildTxKeychain(keyInstance, publicKey);
+
+```
+
+**Run tests**
+
+```
+npm run test
+```
+Add key to your `key_data`:
+```
+keyname: test1@76de427d42c38be4
+password: qwe
+```
+
+![alt image](https://raw.githubusercontent.com/cypherpunk99/web3override/master/screencast.gif)
+
+*TODO bitcoin tests*
---
+++ test/test.js
@@ -0,0 +1,77 @@
+const should = require('chai').should();
+const expect = require('chai').expect;
+
+const Web3 = require('web3');
+
+const API_KEY = 'https://ropsten.infura.io/v3/046804e3dd3240b09834531326f310cf';
+
+let web3 = new Web3(new Web3.providers.HttpProvider(API_KEY));
+
+const to = '0xE8899BA12578d60e4D0683a596EDaCbC85eC18CC';
+const value = 100;
+const data = '';
+const gas = 21000;
+const nonce = 0;
+const gasPrice = 100;
+const chainId = 3;
+
+const transactionParams = {
+  nonce,
+  gasPrice,
+  to,
+  value,
+  data,
+  gas,
+  chainId
+};
+
+describe("Create and sign", () => {
+  let signResKch, signResWeb3, resKch, resWeb3;
+  let selectedKey;
+  const privateKey = '0x920bca893ca29df824858d2e333a159a4d98d1f3c5b5ce76fe236ceb09ae273e';
+  const message = '12345';
+  const Module = require('../lib/index');
+  const { sign, signTransaction } = Module.web3Override(web3);
+  const Keychain = Module.Keychain;
+
+  // it('Select key', async() => {
+  //   const keychain = await Keychain.create();
+  //   const data = await keychain.selectKey();
+  //   selectedKey = data.result;
+  //   console.log('selectedKey: ', selectedKey);
+  //   should.exist(selectedKey);
+  // });
+
+  it('Sign with web3', async () => {
+    signResWeb3 = await web3.eth.accounts.sign(message, privateKey);
+    console.log('signResWeb3: ', signResWeb3);
+    expect(signResWeb3).to.have.property('message');
+  });
+
+  it('Sign transaction with web3', async () => {
+    resWeb3 = await web3.eth.accounts.signTransaction(transactionParams, privateKey);
+    expect(resWeb3).to.have.property('rawTransaction');
+  });
+
+  it('Sign with overridden web3', async () => {
+    web3.eth.accounts.sign = sign;
+    web3.eth.accounts.signTransaction = signTransaction;
+    signResKch = await web3.eth.accounts.sign(message, selectedKey);
+    expect(signResKch).to.have.property('signature');
+  });
+
+  it('Sign transaction with overridden web3', async () => {
+    resKch = await web3.eth.accounts.signTransaction(transactionParams, selectedKey);
+    expect(resKch).to.have.property('rawTransaction');
+  });
+
+  it('Overridden web3 signTransaction valid', async () => {
+    expect(resKch).to.deep.equal(resWeb3);
+  });
+
+  it('Overriden web3 sign valid', async () => {
+    expect(signResKch).to.deep.equal(signResWeb3);
+  });
+
+});
+
