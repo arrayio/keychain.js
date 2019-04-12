2b438a01c50a673a23647fbec0b958f942e93c76
dtaipov <dtaipov@gmail.com>
2019-04-12 18:06:17 +0300

    testRun parameter for tests enabled

--- test/pipe.js
+++ test/pipe.js
@@ -2,6 +2,6 @@ const shared = require('./sharedKeychain');
 const Keychain = require('../lib/pipe');
 
 describe('keychain.js methods with pipe', () => {
-  shared.testKeychain(new Keychain());
+  shared.testKeychain(new Keychain(true));
 });
 
